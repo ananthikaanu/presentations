a90c1eff4f6ca140a9b8ab06a8b8aee5150de1dc
ananthikaanu <106010941+ananthikaanu@users.noreply.github.com>
2023-09-20 08:52:52 +0530

    Add files via upload

---
+++ props/src/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Greeting(props){
+  return (
+    <div>
+        <h1>{props.name}</h1>
+        <h1>{props.age}</h1>
+    </div>
+  )
+}
+
+export default Greeting
+
+//here pass data through properties from parent to child
