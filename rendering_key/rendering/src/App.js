8abf8e4d142ea0c4a311fb8ab25a0243da5e1c35
ananthikaanu <106010941+ananthikaanu@users.noreply.github.com>
2023-09-20 09:47:52 +0530

    Add files via upload

---
+++ rendering_key/rendering/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React,{Component} from "react";
+import Demo  from "./Demo";
+
+  class App extends Component {
+     render() {
+        return (
+        <div className='App'>
+           <Demo/>
+        </div>
+       )
+  }
+}
+export default App
