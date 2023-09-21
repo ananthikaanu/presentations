77fe89ea8a258a38d03cd6ec49935a0847b43cf9
ananthikaanu <106010941+ananthikaanu@users.noreply.github.com>
2023-09-21 21:40:40 +0530

    Delete management directory

--- management/src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button =({label,func}) => {
-    return(
-        <button onClick={() => func()}>{label}</button>
-    )
-}
-
-export default Button
--- management/src/Child.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Child=({count}) => {
-    return(
-        <h1>Counter Value is {count}</h1>
-    )
-}
-
-export default Child;
